53f60b25daa9bc59f6bad39700ee036a833e4a2f
cezanekarki <karkicezane123@gmail.com>
2022-11-04 11:47:59 +0545

    Image generation Cuda not available

---
+++ code/stackgan_model_runner.py
@@ -0,0 +1,74 @@
+from datasets import DatasetLoadTransform
+import os
+from stackgan_runner import GAN
+from utils import mkdir_p
+from configuration import conf, cfg
+import torch.backends.cudnn as cudnn
+import torch
+import torchvision.transforms as transforms
+
+import argparse
+import random
+import sys
+import pprint
+import datetime
+import dateutil
+import dateutil.tz
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file',
+                        default='', type=str)
+    parser.add_argument('--gpu',  dest='gpu_id', type=str, default='0')
+    parser.add_argument('--data_dir', dest='data_dir', type=str, default='')
+    parser.add_argument('--manualSeed', type=int, help='manual seed')
+    args = parser.parse_args()
+    return args
+
+def main():
+    now = datetime.datetime.now(dateutil.tz.tzlocal())
+    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    output_dir = '../output/%s_%s_%s' % \
+                 (cfg.DATASET_NAME, cfg.CONFIG_NAME, timestamp)
+
+    num_gpu = len(cfg.GPU_ID.split(','))
+    if cfg.TRAIN.FLAG:
+        image_transform = transforms.Compose([
+            transforms.RandomCrop(cfg.IMSIZE),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        dataset = DatasetLoadTransform(cfg.DATA_DIR,'train',
+                              imsize=cfg.IMSIZE,
+                              transform=image_transform)
+        assert dataset
+        dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=cfg.TRAIN.BATCH_SIZE * num_gpu,
+            drop_last=True, shuffle=True, num_workers=int(cfg.WORKERS))
+
+        algo = GAN(output_dir)
+        algo.train(dataloader, cfg.STAGE)
+    else:
+        datapath= '%s/test/val_captions.t7' % (cfg.DATA_DIR)
+        algo = GAN(output_dir)
+        algo.sample(datapath, cfg.STAGE)
+    
+    print("Completed")
+
+
+if __name__ == "__main__":
+    args =parse_args()
+    if args.data_dir != '':
+        cfg.DATA_DIR = args.data_dir
+    if args.cfg_file != '':
+        conf(args.cfg_file)
+    if args.gpu_id != -1:
+        cfg.GPU_ID = args.gpu_id
+
+    if args.manualSeed is None:
+        args.manualSeed = random.randint(1, 10000)
+
+    random.seed(args.manualSeed)
+    torch.manual_seed(args.manualSeed)
+    main()
